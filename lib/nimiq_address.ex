9f24fca4c93b6073975db6d9106aabdef0ef99bf
redmaner <jakevdputten@protonmail.com>
2024-02-26 12:26:47 +0100

    Properly parse transaction type from recipientData

--- lib/nimiq_address.ex
+++ lib/nimiq_address.ex
@@ -2,38 +2,65 @@ defmodule NimiqAddress do
   require Logger
   @ccode "NQ"
 
-  def decode_from_stake_data(data) do
+  @doc """
+  decode_recipient_data extracts the transaction type and beneficiary address from `recipientData` which is field
+  present in transactions to the staking contract.
+  """
+  def decode_recipient_data(data) do
     data
     |> :binary.decode_hex()
     |> case do
       data when byte_size(data) == 21 ->
-        data
-        |> binary_part(1, 20)
-        |> to_user_friendly()
+        action_type =
+          data
+          |> binary_part(0, 1)
+          |> extract_recipient_data_type()
+
+        address =
+          data
+          |> binary_part(1, 20)
+          |> to_user_friendly_address()
+
+        {:ok, action_type, address}
 
       _other ->
         {:error, :invalid_data}
     end
   end
 
+  defp extract_recipient_data_type(<<0>>), do: :CREATE_VALIDATOR
+  defp extract_recipient_data_type(<<1>>), do: :UPDATE_VALIDATOR
+  defp extract_recipient_data_type(<<2>>), do: :DEACTIVATE_VALIDATOR
+  defp extract_recipient_data_type(<<3>>), do: :REACTIVATE_VALIDATOR
+  defp extract_recipient_data_type(<<4>>), do: :RETIRE_VALIDATOR
+  defp extract_recipient_data_type(<<5>>), do: :CREATE_STAKER
+  defp extract_recipient_data_type(<<6>>), do: :ADD_STAKE
+  defp extract_recipient_data_type(<<7>>), do: :UPDATE_STAKER
+  defp extract_recipient_data_type(<<8>>), do: :SET_ACTIVE_STAKE
+  defp extract_recipient_data_type(<<9>>), do: :RETIRE_STAKE
+
+  @doc """
+  decode_from_hex decodes a raw hex encoded address to a user friendly address
+  """
   def decode_from_hex(hex) do
     hex
     |> :binary.decode_hex()
-    |> to_user_friendly()
+    |> to_user_friendly_address()
   end
 
-  def to_user_friendly(bytes) do
+  @doc """
+  to_user_friendly_address transforms a raw Nimiq address to the user friendly variant
+  """
+  def to_user_friendly_address(bytes) do
     base32 = NimiqAddress.Base.encode32(bytes)
     iban_check = "00" <> ((98 - iban_check(base32 <> @ccode <> "00")) |> to_string())
     check = String.slice(iban_check, String.length(iban_check) - 2, 2)
 
-    @ccode <> check <> base32
+    (@ccode <> check <> base32)
     |> String.codepoints()
     |> Stream.chunk_every(4)
     |> Stream.map(&Enum.join/1)
-    |> Enum.reduce("", fn chunk, acc ->
-      acc <> " " <> chunk
-    end)
+    |> Enum.reduce("", &(&2 <> " " <> &1))
     |> String.trim()
   end
 
@@ -47,15 +74,13 @@ defmodule NimiqAddress do
         if code >= 48 and code <= 57 do
           c
         else
-          code - 55 |> to_string()
-
+          (code - 55) |> to_string()
         end
       end)
       |> Enum.join()
 
-
-    iterate = Stream.iterate(0, &(&1 + 1))
-    Enum.reduce_while(iterate, "", &reduce_iban_number(&1, &2, num))
+    Stream.iterate(0, &(&1 + 1))
+    |> Enum.reduce_while("", &reduce_iban_number(&1, &2, num))
     |> String.to_integer()
   end
 
